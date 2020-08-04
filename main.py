e881071eb6658f0e026f5d12ab559b3ad0ebc09a
stanlee321 <stanlee321@gmail.com>
2020-08-03 20:13:05 -0400

    * added scriips for read one dropdown and show his csv in pandas way

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
-import sqlite3
 import json
 
-
+import pandas as pd
+from urllib.request import Request, urlopen
+import io
 import os
-from tqdm import tqdm
 import sys
+import time
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 class EleccionesScraper:
 
-    HOME_PATH = os.getenv('HOME')
-    SCRAPED_DATA_DIRECTORY = os.path.join(HOME_PATH, "ELECCIONES") 
-    
-    def __init__(self, headless:bool = True):
+
+    def __init__(self,project_path:str,  headless:bool = True):
         """
         This scraper downloads information from http://atlaselectoral.oep.org.bo/
         """
-        pass
+        # Project path
+
+        self.PROJECT_PATH = project_path
+        self.SCRAPER_PATH = os.path.join(self.PROJECT_PATH)
 
     def _login_custom(self,
                     input_link:str,
@@ -69,14 +67,96 @@ class EleccionesScraper:
             print("There's some error in log in.")
             print(sys.exc_info()[0])
 
-    def main(self, link:str, headless:bool = False):
+    def main(self,  headless:bool = False):
 
-        with open(f"{ self.FB_PROFILE_SCRAPER_FOLDER }/selectors.json") as a:
+        with open(f"{self.SCRAPER_PATH}/selectors.json") as a:
             selectors = json.load(a)
 
         link = selectors.get("base_link")
 
-        driver = self._login_custom(self,
-                    input_link = link,
+        driver = self._login_custom( input_link = link,
                     headless = headless)
+
+        # Perform click on Elecciones Generales.
+
+        time.sleep(2)
+
+        buttons_dropdown_bar = driver.find_elements_by_xpath(
+            selectors.get("buttons_dropdown_bar")
+        )
+
+        print(f"FOUNR {len(buttons_dropdown_bar)}")
+
+        buttons_dropdown_bar[0].click()
+
+        # Find mat-menu-content
         
+        mat_menu_content = driver.find_elements_by_xpath(
+            selectors.get("mat-menu-content")
+        )
+
+        # perform click in each one of the sub dropdown sub elements:
+        sub_buttons_dropdown_bar = mat_menu_content[0].find_elements_by_xpath(
+            selectors.get("sub_buttons_dropdown_bar")
+        )
+
+        print(f"DOWNDOWN COUNTS {len(sub_buttons_dropdown_bar)}")
+
+
+        for dropdown in sub_buttons_dropdown_bar:
+
+            dropdown.click()
+
+            sub_sub_button_dropdown_div = driver.find_elements_by_id(
+                selectors.get("sub_sub_button_dropdown_div")
+            )
+
+            sub_sub_button_dropdown_div[0].click()
+
+            nav_buttons_div = driver.find_elements_by_xpath(
+                selectors.get("buttons_center_div")
+            )
+
+            nav_buttons = nav_buttons_div[0].find_elements_by_xpath(
+                selectors.get("buttons_center_el")
+            )
+            time.sleep(2)
+            
+            nav_buttons[-1].click()
+
+
+            els = driver.find_elements_by_xpath(
+                            selectors.get("data_column")
+            )
+
+            time.sleep(2)
+
+
+            data_links = els[-1].find_elements_by_xpath(
+                            selectors.get("download_votes_els")
+            ) 
+
+            for el in data_links:
+                print(el.text)
+                print(el.get_attribute("href"))
+
+                self.test_read_remote_csv( el.get_attribute("href") )
+
+        time.sleep(5)
+
+    def test_read_remote_csv(self, csv_link):
+
+        response_raw = Request(csv_link, headers={'User-Agent': 'Mozilla/5.0'})
+        response = urlopen(response_raw,  timeout=2).read()
+
+        response = response.decode('utf-8')
+        
+        df =pd.read_csv(io.StringIO(response))
+
+        print(df.head())
+
+if __name__ == "__main__":
+    elecciones = EleccionesScraper(project_path = ".")
+
+    elecciones.main(headless=True)
+    #elecciones.test_read_remote_csv(csv_link = "http://atlaselectoral.oep.org.bo/descarga/52/votos_totales.csv")
